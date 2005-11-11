f9983a08a291c3c36af374c570c9f5da26d05e86
Torsten Curdt <tcurdt@apache.org>
2005-11-11 00:15:55 +0000

    a simple factory implementation
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/jci/trunk@332406 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/jci/compilers/JavaCompilerFactory.java
+++ src/java/org/apache/commons/jci/compilers/JavaCompilerFactory.java
@@ -15,8 +15,66 @@
  */
 package org.apache.commons.jci.compilers;
 
-public interface JavaCompilerFactory {
-    
-    JavaCompiler createCompiler(final Object pHint);
+import org.apache.commons.jci.compilers.eclipse.EclipseJavaCompiler;
+import org.apache.commons.jci.compilers.groovy.GroovyJavaCompiler;
+import org.apache.commons.jci.compilers.janino.JaninoJavaCompiler;
 
+public final class JavaCompilerFactory {
+    
+    public static final int ECLIPSE = 0;
+    public static final int JANINO = 1;
+    public static final int GROOVY = 2;
+    
+    private static final JavaCompilerFactory INSTANCE = new JavaCompilerFactory();
+    
+    public static JavaCompilerFactory getInstance() {
+        return JavaCompilerFactory.INSTANCE;
+    }
+    
+    private JavaCompilerFactory() {
+    }
+    
+    /**
+     * Can accept the following strings "ECLIPSE", "JANINO", "GROOVY" and returns the appropriate
+     * JavaCompiler. Return null for any other type of string.
+     * 
+     * @param compiler
+     * @return
+     */
+    public JavaCompiler createCompiler(final String pHint) {
+        if ("eclipse".equalsIgnoreCase(pHint)) {
+            return createCompiler(ECLIPSE);
+        }
+        if ("janino".equalsIgnoreCase(pHint)) {
+            return createCompiler(JANINO);
+        }
+        if ("groovy".equalsIgnoreCase(pHint)) {
+            return createCompiler(GROOVY);
+        }
+    
+        return null;                       
+    }
+    
+    /**
+     * Can accept the following ints
+     *  JavaCompilerFactory.ECLIPSE
+     *  JavaCompilerFactory.JANINO
+     *  JavaCompilerFactory.GROOVY 
+     * and returns the appropriate JavaCompiler. Return null for any other int.
+     * 
+     * @param compiler
+     * @return
+     */    
+    public JavaCompiler createCompiler(final int pCompiler) {
+        switch (pCompiler) {
+            case ECLIPSE:
+                return new EclipseJavaCompiler();
+            case JANINO:
+                return new JaninoJavaCompiler();
+            case GROOVY:
+                return new GroovyJavaCompiler();
+            default:
+                return null;
+        }
+    }
 }
