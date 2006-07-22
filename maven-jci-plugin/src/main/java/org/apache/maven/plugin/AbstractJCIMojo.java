2f66e3770a7469fa2a2bc5a2c7d2ea9a6955d3a8
Torsten Curdt <tcurdt@apache.org>
2006-07-22 11:26:16 +0000

    GSoC: using javac through jci by Peter Konstantinov
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/jci/trunk@424550 13f79535-47bb-0310-9956-ffa450edef68

--- maven-jci-plugin/src/main/java/org/apache/maven/plugin/AbstractJCIMojo.java
+++ maven-jci-plugin/src/main/java/org/apache/maven/plugin/AbstractJCIMojo.java
@@ -2,6 +2,7 @@ package org.apache.maven.plugin;
 
 import java.io.File;
 import java.util.List;
+import org.apache.commons.jci.compilers.CompilationResult;
 import org.apache.commons.jci.compilers.JavaCompiler;
 import org.apache.commons.jci.compilers.JavaCompilerFactory;
 import org.apache.commons.jci.readers.FileResourceReader;
@@ -21,7 +22,7 @@ public abstract class AbstractJCIMojo extends AbstractMojo
 	 * The compiler id of the compiler to use.
 	 * 
 	 * @parameter expression="${maven.compiler.compilerId}"
-	 *            default-value="eclipse"
+	 *            default-value="javac"
 	 */
 	private String compilerId;
 
@@ -51,8 +52,23 @@ public abstract class AbstractJCIMojo extends AbstractMojo
 
 	public void execute() throws MojoExecutionException, MojoFailureException
 	{
+		getLog().debug(compilerId);
 		JavaCompiler compiler = JavaCompilerFactory.getInstance()
 				.createCompiler(compilerId);
+		logSettings();
+		FileResourceReader fileResourceReader = new FileResourceReader(
+				new File((String) getCompileSourceRoots().get(0)));
+		getOutputDirectory().mkdirs();
+		FileResourceStore fileResourceStore = new FileResourceStore(
+				getOutputDirectory());
+		CompilationResult result = compiler.compile(
+				new String[] { "TestCompile0.java" }, fileResourceReader,
+				fileResourceStore);
+		logResult(result);
+	}
+
+	private void logSettings()
+	{
 		if (getLog().isDebugEnabled())
 		{
 			getLog().debug(
@@ -65,11 +81,17 @@ public abstract class AbstractJCIMojo extends AbstractMojo
 									'\n'));
 			getLog().debug("Output directory: " + getOutputDirectory());
 		}
-		FileResourceReader fileResourceReader = new FileResourceReader(
-				new File((String) getCompileSourceRoots().get(0)));
-		FileResourceStore fileResourceStore = new FileResourceStore(
-				getOutputDirectory());
-		compiler.compile(new String[] { "TestCompile0.java" },
-				fileResourceReader, fileResourceStore);
+	}
+
+	protected void logResult(CompilationResult result)
+	{
+		for (int i = 0; i < result.getErrors().length; i++)
+		{
+			getLog().error(result.getErrors()[i].toString());
+		}
+		for (int i = 0; i < result.getWarnings().length; i++)
+		{
+			getLog().warn(result.getWarnings()[i].toString());
+		}
 	}
 }
