18a363fea10531c2db69e3394d067c9ab2cdb718
Torsten Curdt <tcurdt@apache.org>
2007-03-06 23:37:46 +0000

    fail a test if javac is not in the classpath
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/jci/trunk@515365 13f79535-47bb-0310-9956-ffa450edef68

--- compilers/javac/src/test/java/org/apache/commons/jci/compilers/JavacJavaCompilerTestCase.java
+++ compilers/javac/src/test/java/org/apache/commons/jci/compilers/JavacJavaCompilerTestCase.java
@@ -10,4 +10,16 @@ public class JavacJavaCompilerTestCase extends AbstractCompilerTestCase {
 		return "javac";
 	}
 
+	public void testForToolsJar() {
+		try {
+			Class.forName("com.sun.tools.javac.Main");
+		} catch (ClassNotFoundException e) {
+			final StringBuffer sb = new StringBuffer();
+			sb.append("Could not find javac compiler class (should be in the tools.jar/classes.jar in your JRE/JDK). ");
+			sb.append("os.name").append('=').append(System.getProperty("os.name")).append(", ");
+			sb.append("os.version").append('=').append(System.getProperty("os.version")).append(", ");
+			sb.append("java.class.path").append('=').append(System.getProperty("java.class.path"));
+			fail(sb.toString());
+		}
+	}
 }
