414f52e87f02094b264c7b232990299a052d50ec
Torsten Curdt <tcurdt@apache.org>
2007-03-10 19:38:19 +0000

    example skeletons
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/jci/trunk@516773 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/commons/jci/examples/commandline/CommandlineCompiler.java
+++ examples/src/main/java/org/apache/commons/jci/examples/commandline/CommandlineCompiler.java
@@ -1,20 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.commons.jci.examples.commandline;
 
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-
-import org.apache.commons.jci.compilers.JavaCompiler;
-import org.apache.commons.jci.compilers.JavaCompilerFactory;
-import org.apache.commons.jci.compilers.JavaCompilerSettings;
-import org.apache.commons.jci.readers.FileResourceReader;
-import org.apache.commons.jci.readers.ResourceReader;
-import org.apache.commons.jci.stores.MemoryResourceStore;
-import org.apache.commons.jci.stores.ResourceStore;
-
-
-public final class Compile {
+/**
+ * 
+ * @author tcurdt
+ */
+public final class CommandlineCompiler {
     
     public static void main( String[] args ) throws Exception {
         /*
